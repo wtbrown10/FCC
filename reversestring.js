68c5feadcbf99cf87ef83700684e146cb67e151c
Will Brown <wtbrown10@gmail.com>
2020-04-10 16:00:30 -0400

    rewrote the code with different and it ended up working

--- reversestring.js
+++ reversestring.js
@@ -1,18 +1,27 @@
-function reverseString(str) {
-  let myStr = "";
-  for (let i = str.length - 1; i > -1; i--);
-  {
-    myStr = myStr + str[i];
-  }
+// function reverseString(str) {
 
-  console.log(myStr);
-  return str;
-}
+//   for (let i = str. - 1; i > -1; i--);
+//   {
+//     myStr = myStr + str;
+//   }
 
-reverseString("hello");
+//   console.log(myStr);
+//   return str;
+// }
+
+// reverseString("hello");
 
 //   Reverse the provided string.
 
 //   You may need to turn the string into an array before you can reverse it.
 
 //   Your result must be a string.   .reverse
+
+function switchShitUp(word) {
+  let newWord = "";
+  for (let i = word.length - 1; i > -1; i--) {
+    newWord += word[i];
+  }
+  console.log(newWord);
+}
+switchShitUp("hello");
