0d1cf34bd84301f39f2764bbb8131e0997a212cd
Will Brown <wtbrown10@gmail.com>
2020-04-14 02:37:58 -0400

    comparisons with factorialize

---
+++ comparisonswith.js
@@ -0,0 +1,26 @@
+function factorialize(num) {
+    for (var product = 1; 0 < num; num--) {
+        product *= num
+    }
+    return product;
+}
+
+console.log(factorialize(5));
+
+
+
+
+
+
+
+
+//   Basic Algorithm Scripting: Factorialize a Number
+//   Return the factorial of the provided integer.
+
+//   If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+
+//   Factorials are often represented with the shorthand notation n!
+
+//   For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+//   Only integers greater than or equal to zero will be supplied to the function. 
