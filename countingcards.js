7685ea3d5f474abca105b9bfe133ec43930fb1e8
Will Brown <wtbrown10@gmail.com>
2020-04-14 20:20:04 -0400

    created function to count cards in black jack

---
+++ countingcards.js
@@ -0,0 +1,37 @@
+var count = 0;
+
+function cc(card) {
+    // Only change code below this line
+    switch (card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count++
+            break;
+
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count--
+            break;
+    }
+    if (count > 0) {
+        return count + " Bet!"
+    } else {
+        return count + " Hold"
+    }
+
+    return;
+    // Only change code above this line
+}
+
+cc(2); cc(3); cc(5); cc('K'); cc('A');
+console.log(cc(5))
+
+
+
+// You will write a card counting function. It will receive a card parameter, which can be a number or a string, and increment or decrement the global count variable according to the card's value (see table). The function will then return a string with the current count and the string Bet if the count is positive, or Hold if the count is zero or negative. The current count and the player's decision (Bet or Hold) should be separated by a single space.
