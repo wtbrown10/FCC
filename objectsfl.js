02b7c6979d49c2a1973771ca76af6e00ed1eb170
Will Brown <wtbrown10@gmail.com>
2020-04-22 23:24:46 -0400

    record collection

--- objectsfl.js
+++ objectsfl.js
@@ -17,16 +17,3 @@ function phoneticLookup(val) {
 }
 
 console.log(phoneticLookup("charlie"));
-
-function checkObj(obj, checkProp) {
-    // Only change code below this line
-
-    if (obj.hasOwnProperty(checkProp) == true) {
-        return obj[checkProp]
-    } else {
-        return "Not Found"
-    }
-    // Only change code above this line
-}
-
-console.log(checkObj({ gift: "pony", pet: "kitten", bed: "sleigh" }, "gift"))
