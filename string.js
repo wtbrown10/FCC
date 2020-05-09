4580be556e69d3a0098c8a7b59fa55f1a62c08fb
Will Brown <wtbrown10@gmail.com>
2020-05-08 22:18:07 -0400

    string

---
+++ string.js
@@ -0,0 +1,10 @@
+function reverseString(str) {
+  let reverse = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse = reverse + str[i];
+  }
+  let string = JSON.stringify(reverse);
+  console.log(string);
+}
+
+reverseString("hello");
